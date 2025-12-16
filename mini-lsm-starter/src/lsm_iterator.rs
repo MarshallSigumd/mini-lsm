af10bc42ead23e05bc3f0f0b604035f1990b9eda
dlzwtk <2020694283@qq.com>
2025-12-17 00:50:28 +0800

    week1 day2 task3 pass

--- mini-lsm-starter/src/lsm_iterator.rs
+++ mini-lsm-starter/src/lsm_iterator.rs
@@ -15,7 +15,7 @@
 #![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
 #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 
-use anyhow::Result;
+use anyhow::{Ok, Result, bail};
 
 use crate::{
     iterators::{StorageIterator, merge_iterator::MergeIterator},
@@ -31,7 +31,20 @@ pub struct LsmIterator {
 
 impl LsmIterator {
     pub(crate) fn new(iter: LsmIteratorInner) -> Result<Self> {
-        Ok(Self { inner: iter })
+        let mut iter = Self { inner: iter }; //此时iter可能不合法可能是墓碑
+        iter.move_to_non_delete()?;
+        Ok(iter)
+    }
+}
+
+//将迭代器向前移动，跳过所有被标记为删除的条目（墓碑记录），直到找到有效的非删除条目或到达迭代器末尾
+impl LsmIterator {
+    fn move_to_non_delete(&mut self) -> Result<()> {
+        //is_vaild()检查是不是有效，is_emptu()检查是否为墓碑
+        while self.is_valid() && self.inner.value().is_empty() {
+            self.inner.next()?;
+        }
+        Ok(())
     }
 }
 
@@ -39,25 +52,34 @@ impl StorageIterator for LsmIterator {
     type KeyType<'a> = &'a [u8];
 
     fn is_valid(&self) -> bool {
-        unimplemented!()
+        self.inner.is_valid()
     }
 
     fn key(&self) -> &[u8] {
-        unimplemented!()
+        self.inner.key().raw_ref()
     }
 
     fn value(&self) -> &[u8] {
-        unimplemented!()
+        self.inner.value()
     }
 
+    //用户在迭代器无效时不应调用 key 、 value 或 next 。同时，如果 next 返回错误，用户也不应再使用该迭代器。
     fn next(&mut self) -> Result<()> {
-        unimplemented!()
+        self.inner.next()?;
+        self.move_to_non_delete()?;
+        Ok(())
     }
 }
 
 /// A wrapper around existing iterator, will prevent users from calling `next` when the iterator is
 /// invalid. If an iterator is already invalid, `next` does not do anything. If `next` returns an error,
 /// `is_valid` should return false, and `next` should always return an error.
+///
+/// FusedIterator 是一个包装器结构体，用于为迭代器提供熔断（fused）行为，防止在迭代器无效或出错后继续使用导致未定义行为。
+/// 核心特性：
+///防止重复调用：在迭代器无效时调用 next() 不会产生意外行为
+/// 错误状态锁定：一旦出错，迭代器永久处于错误状态
+/// 安全边界：为不安全的底层迭代器提供安全包装
 pub struct FusedIterator<I: StorageIterator> {
     iter: I,
     has_errored: bool,
@@ -79,18 +101,34 @@ impl<I: StorageIterator> StorageIterator for FusedIterator<I> {
         Self: 'a;
 
     fn is_valid(&self) -> bool {
-        unimplemented!()
+        !self.has_errored && self.iter.is_valid()
     }
 
     fn key(&self) -> Self::KeyType<'_> {
-        unimplemented!()
+        if self.has_errored || !self.iter.is_valid() {
+            panic!("iterator has errored");
+        }
+        self.iter.key()
     }
 
     fn value(&self) -> &[u8] {
-        unimplemented!()
+        if self.has_errored || !self.iter.is_valid() {
+            panic!("iterator has errored");
+        }
+        self.iter.value()
     }
 
     fn next(&mut self) -> Result<()> {
-        unimplemented!()
+        // only move when the iterator is valid and not errored
+        if self.has_errored {
+            bail!("the iterator is tainted");
+        }
+        if self.iter.is_valid() {
+            if let Err(e) = self.iter.next() {
+                self.has_errored = true;
+                return Err(e);
+            }
+        }
+        Ok(())
     }
 }
