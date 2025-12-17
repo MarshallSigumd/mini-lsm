cc58a1d02a1b352c3b7ca7178efebd51647388cf
dlzwtk <2020694283@qq.com>
2025-12-17 22:54:52 +0800

    week1 day3 task2 pass

--- mini-lsm-starter/src/block/iterator.rs
+++ mini-lsm-starter/src/block/iterator.rs
@@ -20,6 +20,8 @@ use std::sync::Arc;
 use crate::key::{KeySlice, KeyVec};
 
 use super::Block;
+use crate::block::SIZEOF_U16;
+use bytes::Buf;
 
 /// Iterates on a block.
 pub struct BlockIterator {
@@ -28,11 +30,10 @@ pub struct BlockIterator {
     /// The current key, empty represents the iterator is invalid
     key: KeyVec,
     /// the current value range in the block.data, corresponds to the current key
+    /// 就是当前key对应的value在块中的起始位置和结束位置
     value_range: (usize, usize),
     /// Current index of the key-value pair, should be in range of [0, num_of_elements)
     idx: usize,
-    /// The first key in the block
-    first_key: KeyVec,
 }
 
 impl BlockIterator {
@@ -42,50 +43,99 @@ impl BlockIterator {
             key: KeyVec::new(),
             value_range: (0, 0),
             idx: 0,
-            first_key: KeyVec::new(),
         }
     }
 
     /// Creates a block iterator and seek to the first entry.
     pub fn create_and_seek_to_first(block: Arc<Block>) -> Self {
-        unimplemented!()
+        let mut iter = Self::new(block);
+        iter.seek_to_first();
+        iter
     }
 
     /// Creates a block iterator and seek to the first key that >= `key`.
     pub fn create_and_seek_to_key(block: Arc<Block>, key: KeySlice) -> Self {
-        unimplemented!()
+        let mut iter = Self::new(block);
+        iter.seek_to_key(key);
+        iter
     }
 
     /// Returns the key of the current entry.
     pub fn key(&self) -> KeySlice {
-        unimplemented!()
+        debug_assert!(!self.key.is_empty(), "iterator invaild!");
+        self.key.as_key_slice()
     }
 
     /// Returns the value of the current entry.
     pub fn value(&self) -> &[u8] {
-        unimplemented!()
+        debug_assert!(!self.key.is_empty(), "invalid iterator");
+        &self.block.data[self.value_range.0..self.value_range.1]
     }
 
     /// Returns true if the iterator is valid.
     /// Note: You may want to make use of `key`
     pub fn is_valid(&self) -> bool {
-        unimplemented!()
+        !self.key.is_empty()
     }
 
     /// Seeks to the first key in the block.
     pub fn seek_to_first(&mut self) {
-        unimplemented!()
+        self.seek_to_idx(0);
     }
 
     /// Move to the next key in the block.
     pub fn next(&mut self) {
-        unimplemented!()
+        self.idx += 1;
+        self.seek_to_idx(self.idx)
+    }
+
+    /// Seeks to the idx-th key in the block.
+    fn seek_to_idx(&mut self, idx: usize) {
+        if idx >= self.block.offsets.len() {
+            self.key.clear();
+            self.value_range = (0, 0);
+            return;
+        }
+        let offset = self.block.offsets[idx] as usize;
+        self.seek_to_offset(offset);
+        self.idx = idx;
+    }
+
+    /// Seek to the specified position and update the current `key` and `value`
+    /// Index update will be handled by caller
+    /// 将迭代器定位到 Block 数据中的指定偏移量位置，解析该位置的键值对，并更新迭代器的状态（key 和 value_range）
+    fn seek_to_offset(&mut self, offset: usize) {
+        let mut entry = &self.block.data[offset..];
+        // Since `get_u16()` will automatically move the ptr 2 bytes ahead here,
+        // we don't need to manually advance it
+        let key_len = entry.get_u16() as usize;
+        let key = entry[..key_len].to_vec();
+        entry.advance(key_len);
+        self.key.clear();
+        self.key = KeyVec::from_vec(key);
+        let value_len = entry.get_u16() as usize;
+        let value_offset_begin = offset + SIZEOF_U16 + key_len + SIZEOF_U16;
+        let value_offset_end = value_offset_begin + value_len;
+        self.value_range = (value_offset_begin, value_offset_end);
+        entry.advance(value_len);
     }
 
     /// Seek to the first key that >= `key`.
     /// Note: You should assume the key-value pairs in the block are sorted when being added by
     /// callers.
     pub fn seek_to_key(&mut self, key: KeySlice) {
-        unimplemented!()
+        let mut idx_low = 0;
+        let mut idx_high = self.block.offsets.len();
+        while idx_low < idx_high {
+            let mid = idx_low + (idx_high - idx_low) / 2;
+            self.seek_to_idx(mid);
+            assert!(self.is_valid());
+            match self.key().cmp(&key) {
+                std::cmp::Ordering::Equal => return,
+                std::cmp::Ordering::Greater => idx_high = mid,
+                std::cmp::Ordering::Less => idx_low = mid + 1,
+            }
+        }
+        self.seek_to_idx(idx_low);
     }
 }
